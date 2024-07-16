769e685eb168a54466f654eaee5778a3e45d2c71
Adhitya Logan <adhityalogan@gmail.com>
2024-07-15 17:10:27 -0700

    add descriptions

--- report/rollingaverage/rollingaverage.php
+++ report/rollingaverage/rollingaverage.php
@@ -85,6 +85,7 @@ function getObjectsbyEntity()
 
          if (count($query) > 0) {
             if (!$display_header) {
+               echo "<div class='alert alert-primary mt-3 text-center'>This report lists the rolling average of each group's asset reservation over 9 weeks. Each data point is an average of the previous 9 weeks</div>";
                echo "<br><table class='tab_cadre_fixehov'>";
                echo "<tr><th class='center'>" . __('Group') . "</th>";
                $week_dates = calculateWeekDates();
